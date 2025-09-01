22e25d9e2d7c8fef00ee5d155520d91b6a95cc08
ChristianPohlschneider <c.pohlschneider@t-online.de>
2025-09-01 21:59:48 +0200

    boss attack

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -62,21 +62,28 @@ class Endboss extends MovableObject {
         'img/2.Enemy/3 Final Enemy/Dead/Mesa de trabajo 2 copia 8.png',
         'img/2.Enemy/3 Final Enemy/Dead/Mesa de trabajo 2 copia 9.png',
         'img/2.Enemy/3 Final Enemy/Dead/Mesa de trabajo 2 copia 10.png',
+    ];
 
+    IMAGES_ATTACK = [
+        'img/2.Enemy/3 Final Enemy/Attack/1.png',
+        'img/2.Enemy/3 Final Enemy/Attack/2.png',
+        'img/2.Enemy/3 Final Enemy/Attack/3.png',
+        'img/2.Enemy/3 Final Enemy/Attack/4.png',
+        'img/2.Enemy/3 Final Enemy/Attack/5.png',
+        'img/2.Enemy/3 Final Enemy/Attack/6.png',
     ];
 
     constructor(world, bossSpawnCoordinateX) {
-        
         super().loadImage(this.IMAGES_SPAWNING[0]);
         this.loadImages(this.IMAGES_SPAWNING);
         this.loadImages(this.IMAGES_SWIMMING);
+        this.loadImages(this.IMAGES_ATTACK);
         this.loadImages(this.IMAGES_DIE);
         this.loadImages(this.IMAGES_HURT);
         this.world = world;
         this.x = bossSpawnCoordinateX;
         this.speed = 0;
         this.animate();
-        
     }
 
     animate() {
@@ -88,34 +95,128 @@ class Endboss extends MovableObject {
             } else if (this.isHurt()) {
                 this.playAnimation(this.IMAGES_HURT);
             } else {
-                
+
                 if (spawnID < 8) {
                     this.playAnimation(this.IMAGES_SPAWNING);
                 } else {
-                this.playAnimation(this.IMAGES_SWIMMING);
-                this.isSwimming = true;
+                    this.playAnimation(this.IMAGES_SWIMMING);
+                    this.isSwimming = true;
                 }
-                 spawnID++
-            }      
+                spawnID++
+            }
         }, 200);
 
         this.world.setStoppableInterval(() => {
-        if (this.isSwimming) {
-            this.randomMoveID = Math.floor(Math.random() * 4);
-            this.randomAttack(this.randomMoveID);
-        }
-         }, 10000);
+            if (this.isSwimming) {
+                this.randomMoveID = Math.floor(Math.random() * 4);
+                this.randomAttack(this.randomMoveID);
+            }
+        }, 4000);
     }
 
     randomAttack(randomMoveID) {
+        randomMoveID = 1
         if (randomMoveID == 1) {
             console.log("randomID = 1: " + randomMoveID);
+
+
+            //enemy attackes and moves fast forward, this.x - 400 and then back
+            this.attackMove();
+
         } else if (randomMoveID == 2) {
             console.log("randomID = 2: " + randomMoveID);
+
+
+            //enemy moves this.y down -400 and then back
+            this.verticalMoveDown();
+
         } else if (randomMoveID == 3) {
             console.log("randomID = 3: " + randomMoveID);
+
+
+            //enemy moves this.y down -400 and then back
+            this.verticalMoveUp();
+
         }
 
+    }
+
+    // Bewegung 1: Angriff vorwärts und zurück
+attackMove() {
+    const distance = -400;
+    const speed = 15; // Geschwindigkeit
+    let moved = 0;
+    let forward = true;
+
+    const interval = setInterval(() => {
+        if (forward) {
+            this.x -= speed;
+            moved -= speed;
+
+            // Animation nur während forward
+            
+            this.playAnimation(this.IMAGES_ATTACK);
+
+            if (moved <= distance) {
+                forward = false; // Richtung umkehren
+            }
+        } else {
+            // Rückwärtsbewegung ohne Animation
+            this.x += speed;
+            moved += speed;
+
+            if (moved >= 0) {
+                clearInterval(interval); // Bewegung abgeschlossen
+            }
+        }
+    }, 50);
+    
+}
+
+    // Bewegung 2: nach unten und zurück
+    verticalMoveDown() {
+        this.playAnimation(this.IMAGES_SWIMMING);
+
+        const startY = this.y;
+        const distance = 200;
+        const speed = 20;
+        let moved = 0;
+        let down = true;
+
+        const interval = setInterval(() => {
+            if (down) {
+                this.y += speed;
+                moved += speed;
+                if (moved >= distance) down = false;
+            } else {
+                this.y -= speed;
+                moved -= speed;
+                if (moved <= 0) clearInterval(interval);
+            }
+        }, 50);
+    }
+
+    // Bewegung 3: nach oben und zurück
+    verticalMoveUp() {
+        this.playAnimation(this.IMAGES_SWIMMING);
+
+        const startY = this.y;
+        const distance = 200;
+        const speed = 20;
+        let moved = 0;
+        let up = true;
+
+        const interval = setInterval(() => {
+            if (up) {
+                this.y -= speed;
+                moved += speed;
+                if (moved >= distance) up = false;
+            } else {
+                this.y += speed;
+                moved -= speed;
+                if (moved <= 0) clearInterval(interval);
+            }
+        }, 50);
     }
 
     isDead() {
